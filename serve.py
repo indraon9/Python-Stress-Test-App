bc15d9939ab9713335141d133900e97c68a974c7
indraon9 <34839396+indraon9@users.noreply.github.com>
2023-05-10 01:07:16 -0500

    Create serve.py

---
+++ serve.py
@@ -0,0 +1,22 @@
+from flask import Flask, redirect, url_for, request
+import json
+import socket
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['POST'])
+def store():
+    p = subprocess.Popen(["python", "stress_cpu.py"], shell=True)
+    return "success"
+
+@app.route('/', methods = ['GET'])
+def fetch():
+    ip = socket.gethostname()
+    return ip
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=8080, debug = True)
+    #app.run(debug = True)
+    print(app.url_map)
