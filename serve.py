051b041f2dc7d2ec13ce9f9ef6555fa5bcc16056
indraon9 <34839396+indraon9@users.noreply.github.com>
2023-05-10 22:04:19 -0500

    Update serve.py

--- serve.py
+++ serve.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['POST'])
 def store():
-    p = subprocess.Popen(["python", "stress_cpu.py"], shell=True)
+    p = subprocess.Popen(["python3", "stress_cpu.py"], shell=True)
     return "success"
 
 @app.route('/', methods = ['GET'])
@@ -18,5 +18,5 @@ def fetch():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8080, debug = True)
-    #app.run(debug = True)
+    app.run(debug = True)
     print(app.url_map)
